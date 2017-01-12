4f070ccda43c26b52d8705fd3e1d1cbc12d400b2
Brian H <bheptin@gmail.com>
2017-01-12 13:35:29 -0500

    playerDrop added

--- tetris.js
+++ tetris.js
@@ -3,14 +3,19 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ];
+
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -25,4 +30,41 @@ function drawMatrix(matrix, offset) {
   });
 }
 
-drawMatrix(matrix, {x: 5, y: 5});
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update (time = 0) {
+      const deltaTime = time - lastTime;
+      lastTime = time;
+
+      dropCounter += deltaTime;
+      if (dropCounter > dropInterval) {
+        playerDrop();
+      }
+
+      draw();
+      requestAnimationFrame(update);
+}
+
+const player = {
+    pos: {x: 5, y: 5},
+    matrix: matrix,
+}
+
+document.addEventListener('keydown', event => {
+      if (event.keyCode === 37) {
+        player.pos.x--;
+      } else if (event.keyCode === 39) {
+        player.pos.x++;
+      } else if (event.keyCode === 40) {
+        playerDrop()
+      }
+});
+
+update();
